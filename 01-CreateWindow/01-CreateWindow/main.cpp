ba7ac9164024e975892fa3b842841c7b48ef9b7f
Benny Franco <bennyfranco@icloud.com>
2017-12-21 12:59:38 -0800

    comments of lines

--- 01-CreateWindow/01-CreateWindow/main.cpp
+++ 01-CreateWindow/01-CreateWindow/main.cpp
@@ -7,16 +7,17 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwInit(); // Initialize GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Set major version of OpenGL
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Set minor version of OpenGl
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use core profile of OpenGL
 
 
 #ifdef MACOS
-	glfwWindowHint(GLFW_OPENGL_FOWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_FOWARD_COMPAT, GL_TRUE); // Need it for macOS
 #endif
 
+	// Create the window object and check if this is valid before continue
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
 	if (window == NULL)
 	{
@@ -25,21 +26,33 @@ int main()
 		return -1;
 	}
 
+	// Creates the window context
 	glfwMakeContextCurrent(window);
 
+	// Setting the coordinates, We do have to tell GLFW we want to call this function on 
+	// every window resize by registering it
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// Main render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Checks if any events are triggered (like keyboard input or mouse movement events), updates the window state, and calls the corresponding functions (which we can set via callback methods). 
 		glfwSwapBuffers(window);
+
+		//  Will swap the color buffer (a large buffer that contains color values for each pixel in GLFW's window) that has been used to draw in during this iteration and show it as output to the screen.
 		glfwPollEvents();
 	}
 
+	// Clean and delete all resources allocated.
 	glfwTerminate();
 	return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+	// Behind the scenes OpenGL uses the data specified via glViewport to transform the 2D coordinates it processed 
+	// to coordinates on your screen. For example, a processed point of location (-0.5,0.5) would (as its final
+	// transformation) be mapped to (200,450) in screen coordinates. Note that processed coordinates in OpenGL are 
+	// between -1 and 1 so we effectively map from the range (-1 to 1) to (0, 800) and (0, 600).
 	glViewport(0, 0, width, height);
 }
