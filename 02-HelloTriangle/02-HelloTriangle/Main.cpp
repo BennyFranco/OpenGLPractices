8cc4761e5410e1ab0bddd834e94cdfec961b404e
Benny Franco <bennyfranco@icloud.com>
2017-12-21 17:48:52 -0800

    Problems with dll solved, vertex shader added

--- 02-HelloTriangle/02-HelloTriangle/Main.cpp
+++ 02-HelloTriangle/02-HelloTriangle/Main.cpp
@@ -1,29 +1,41 @@
-
 #define GLEW_STATIC
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 
+#define WIDTH 800
+#define HEIGHT 600
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+
+const char* vertexShaderSource =
+"#version 330 core \n" 
+"layout (location = 0) in vec3 aPos; \n"
+"void main() \n"
+"{ gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); } \n";
+
 
 int main()
 {
 	glewExperimental = GL_TRUE;
 
-	glfwInit();
+	if (!glfwInit()) {
+		std::cerr << "GL initialization failed" << std::endl;
+		return 1;
+	}
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Set major version of OpenGL
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Set minor version of OpenGl
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	
-#ifdef MACOS
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use core profile of OpenGL
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+#ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FOWARD_COMPAT, GL_TRUE); // Need it for macOS
 #endif
 
-	// Create the window object and check if this is valid before continue
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
+														// Create the window object and check if this is valid before continue
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Window", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW Window" << std::endl;
@@ -34,9 +46,40 @@ int main()
 	// Creates the window context
 	glfwMakeContextCurrent(window);
 
+	// Initialize GLEW, NOTE: This must be after create context
+	glewInit();
+
 	// Setting the coordinates, We do have to tell GLFW we want to call this function on 
 	// every window resize by registering it
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Triangle vertices
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
 
 	// Main render loop
 	while (!glfwWindowShouldClose(window))
@@ -48,7 +91,7 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set the clear color
 		glClear(GL_COLOR_BUFFER_BIT); // Clear the passed buffer
 
-		// Checks if any events are triggered (like keyboard input or mouse movement events), updates the window state, and calls the corresponding functions (which we can set via callback methods). 
+									  // Checks if any events are triggered (like keyboard input or mouse movement events), updates the window state, and calls the corresponding functions (which we can set via callback methods). 
 		glfwSwapBuffers(window);
 
 		//  Will swap the color buffer (a large buffer that contains color values for each pixel in GLFW's window) that has been used to draw in during this iteration and show it as output to the screen.
@@ -70,9 +113,9 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 /*
-	We also want to have some form of input control in GLFW and we can achieve this several of GLFW's input functions. 
-	We'll be using GLFW's glfwGetKey function that takes the window as input together with a key. 
-	The function returns whether this key is currently being pressed.
+We also want to have some form of input control in GLFW and we can achieve this several of GLFW's input functions.
+We'll be using GLFW's glfwGetKey function that takes the window as input together with a key.
+The function returns whether this key is currently being pressed.
 */
 void processInput(GLFWwindow *window)
 {
