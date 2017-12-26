23492202dbf515d6687ad4208d701c6ecb492549
Benny Franco <bennyfranco@icloud.com>
2017-12-26 15:00:37 -0800

    Textures solution added

---
+++ 04-Textures/04-Textures/Main.cpp
@@ -0,0 +1,177 @@
+#define GLEW_STATIC
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "Shader.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const char* vertexShaderSource =
+"#version 330 core \n" 
+"layout (location = 0) in vec3 aPos; \n"	// The position variable has attribute position 0
+"layout (location = 1) in vec3 aColor; \n"	// The color variable has attribute position 1
+"out vec3 ourColor; \n" // output a color to the fragment shader
+"void main() \n"
+"{ \n"
+	"gl_Position = vec4(aPos, 1.0); \n"
+	"ourColor = aColor; \n"
+"} \n";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor; \n"
+"in vec3 ourColor; \n"
+"void main() \n"
+"{ FragColor = vec4(ourColor, 1.0); } \n";
+
+int main()
+{
+	glewExperimental = GL_TRUE;
+
+	if (!glfwInit()) {
+		std::cerr << "GL initialization failed" << std::endl;
+		return 1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Set major version of OpenGL
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Set minor version of OpenGl
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use core profile of OpenGL
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FOWARD_COMPAT, GL_TRUE); // Need it for macOS
+#endif
+														// Create the window object and check if this is valid before continue
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Window", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Creates the window context
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW, NOTE: This must be after create context
+	glewInit();
+
+	// Setting the coordinates, We do have to tell GLFW we want to call this function on 
+	// every window resize by registering it
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Triangle vertices
+	/* float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	}; */
+
+	// Triangle vertices with colors
+	float vertices[] = {
+		0.5f, -0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	// bottom right
+	   -0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	// bottom left
+		0.0f,  0.5f, 0.0f,	0.0f, 0.0f, 1.0f	// top
+	};
+
+	// Rectangle vertices
+	/*float vertices[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};*/
+
+	/*GLuint indices[] = {
+		0,1,3, //first triangle
+		1,2,3  //second triangle
+	};*/
+
+	Shader ourShader("./triangleShader.vs", "./triangleShader.fs");
+
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+
+	glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// Main render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		//Call processInput() method
+		processInput(window);
+
+		// Rendering commands
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set the clear color
+		glClear(GL_COLOR_BUFFER_BIT); // Clear the passed buffer
+
+		ourShader.use();
+
+		//glUseProgram(shaderProgram);
+
+		// Used for uniforms example
+		/*float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);*/
+
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
+
+		//  Will swap the color buffer (a large buffer that contains color values for each pixel in GLFW's window) that has been used to draw in during this iteration and show it as output to the screen.
+		glfwPollEvents();
+	}
+
+	// Clean and delete all resources allocated.
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// Behind the scenes OpenGL uses the data specified via glViewport to transform the 2D coordinates it processed 
+	// to coordinates on your screen. For example, a processed point of location (-0.5,0.5) would (as its final
+	// transformation) be mapped to (200,450) in screen coordinates. Note that processed coordinates in OpenGL are 
+	// between -1 and 1 so we effectively map from the range (-1 to 1) to (0, 800) and (0, 600).
+	glViewport(0, 0, width, height);
+}
+
+/*
+We also want to have some form of input control in GLFW and we can achieve this several of GLFW's input functions.
+We'll be using GLFW's glfwGetKey function that takes the window as input together with a key.
+The function returns whether this key is currently being pressed.
+*/
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
---
+++ 04-Textures/04-Textures/Shader.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	GLuint ID;		// the program ID
+
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath); // Constructor reads and builds the shader
+	void use(); // use/active the shader
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string &name, int value) const;
+	void setFloat(const std::string &name, float value) const;
+};
+#endif // SHADER_H
