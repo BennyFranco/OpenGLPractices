ef4925313c1ec1a5dd5e59f7d7aac1d179c23998
Benny Franco <bennyfranco@icloud.com>
2017-12-27 11:58:50 -0800

    Mixin ready

--- 04-Textures/04-Textures/Main.cpp
+++ 04-Textures/04-Textures/Main.cpp
@@ -100,6 +100,9 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, nrChannels;
+
+	stbi_set_flip_vertically_on_load(true);
+
 	unsigned char *data = stbi_load("./images/container.jpg", &width, &height, &nrChannels, 0); 
 	if (data)
 	{
@@ -121,9 +124,10 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	data = stbi_load("./images/awesomeface.png", &width, &height, &nrChannels, 0);
+	
 	if (data)
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 	else
