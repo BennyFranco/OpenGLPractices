59c0fcf9972c2a6bac04d074aaac2ccceba1a7e0
Benny Franco <bennyfranco@icloud.com>
2018-01-02 15:23:09 -0800

    Materials struct

--- 10-Materials/Main.cpp
+++ 10-Materials/Main.cpp
@@ -167,6 +167,11 @@ int main()
 		ourShader.setVec3("lightPos", lightPos);
 		ourShader.setVec3("viewPos", camera.Position);
 
+		ourShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+		ourShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+		ourShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+		ourShader.setFloat("material.shininess", 32.0f);
+
 		glm::mat4 model;
 		model = glm::translate(model, cubePositions[0]);
 
--- 10-Materials/triangleShader.fs
+++ 10-Materials/triangleShader.fs
@@ -1,4 +1,13 @@
 #version 330 core
+
+struct Material
+{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
 out vec4 FragColor;
 
 in vec3 Normal;
@@ -9,26 +18,25 @@ uniform vec3 lightColor;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
+uniform Material material;
+
 void main()
 { 
-    float ambientStrenght = 0.1;
-    float specularStrenght = 0.5;
+    // ambient
+    vec3 ambient = lightColor * material.ambient;
 
-    vec3 ambient = ambientStrenght * lightColor;
-
+    // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
-
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = lightColor * (diff * material.diffuse);
 
+    // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-
     float spec = pow(max(dot(viewDir, reflectDir),0.0), 32);
-    vec3 specular = specularStrenght * spec * lightColor;
+    vec3 specular = lightColor * (spec * material.specular);
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    
+    vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
