305ec65d367cf2a3ffff3759b64152d1bb7af2ed
Benny Franco <bennyfranco@icloud.com>
2018-01-03 19:04:21 -0800

    Assimp lib added and mesh-models project created

---
+++ 14-MeshAndModel/Mesh.h
@@ -0,0 +1,42 @@
+#ifndef MESH_H
+#define MESH_H
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <string>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+#include "Shader.h"
+
+struct Vertex
+{
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture
+{
+    GLuint id;
+    std::string type;    
+};
+
+class Mesh
+{
+public:
+    std::vector<Vertex>         vertices;
+    std::vector<GLuint>   indices;
+    std::vector<Texture>        textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, std::vector<Texture> textures);
+    void Draw(Shader shader);
+
+private:
+    GLuint VAO, VBO, EBO;
+
+    void setupMesh();
+};
+#endif // MESH_H
