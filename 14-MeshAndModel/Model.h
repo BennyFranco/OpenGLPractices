f43234d9f967d728e1fa90843b7295437ae27c00
Benny Franco <bennyfranco@icloud.com>
2018-01-03 19:25:20 -0800

    Model header updated

---
+++ 14-MeshAndModel/Model.h
@@ -0,0 +1,26 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <vector>
+#include <string>
+
+#include "Shader.h"
+#include "Mesh.h"
+
+class Model
+{
+public:
+    Model(char* path);
+    void Draw(Shader shader);
+
+private:
+    std::vector<Mesh>   meshes;
+    std::string         directory;
+
+    void loadModel(std::string path);
+    void processNode(aiNode *node, const aiScene *scene);
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
+};
+
+#endif // MODEL_H
