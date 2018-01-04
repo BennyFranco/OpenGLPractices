ff9929c921bf5467467e3f136ae6aa0f9a471e78
Benny Franco <bennyfranco@icloud.com>
2018-01-03 19:14:04 -0800

    Mesh class

--- 14-MeshAndModel/Mesh.cpp
+++ 14-MeshAndModel/Mesh.cpp
@@ -9,7 +9,7 @@ Mesh::Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, std::vecto
     setupMesh();
 }
 
-Mesh::setupMesh()
+void Mesh::setupMesh()
 {
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -35,5 +35,34 @@ Mesh::setupMesh()
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
 
+    glBindVertexArray(0);
+}
+
+void Mesh::Draw(Shader shader)
+{
+    GLuint diffuseNr = 1;
+    GLuint specularNr = 1;
+
+    for(GLuint i = 0; i < textures.size(); i++)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+
+        std::string number;
+        std::string name = textures[i].type
+
+        if(name == "texture_diffuse")
+            number = std::to_string(diffuseNr++);
+        elseif(name == "texture_specular")
+            number = std::to_string(specularNr++);
+        
+        shader.setFloat(("material."+name+number).c_str(), i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+
+    glActiveTexture(GL_TEXTURE0);
+
+    // Draw mesh
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
