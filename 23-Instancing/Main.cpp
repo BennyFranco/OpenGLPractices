78f050620d1b323018ee464eba1518140efc208a
Benny Franco <bennyfranco@icloud.com>
2018-04-12 16:03:02 -0700

    Instancing using glEnableVertexAttribArray

--- 23-Instancing/Main.cpp
+++ 23-Instancing/Main.cpp
@@ -101,15 +101,13 @@ int main()
 		}
 	}
 
-	shader.use();
-	for(unsigned int i = 0; i<100; i++)
-	{
-		std::stringstream ss;
-		std::string index;
-		ss << i;
-		index = ss.str();
-		shader.setVec2(("offsets["+index+"]").c_str(), translations[i]);
-	}
+	unsigned int instanceVBO;
+	glGenBuffers(1, &instanceVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	
 
     unsigned int VBO, VAO;
     glGenBuffers(1, &VBO);
@@ -121,7 +119,11 @@ int main()
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
-    glBindVertexArray(0);  
+    glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribDivisor(2, 1);
 	
 	
 // Main render loop                                                  
