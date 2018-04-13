d1762ed881306d044c9636b162d971582a5a48fe
Benny Franco <bennyfranco@icloud.com>
2018-04-12 18:58:14 -0700

    Asteroids parcial

---
+++ 23.1-Asteroids/Model.h
@@ -0,0 +1,30 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <vector>
+#include <string>
+
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+
+#include "Shader.h"
+#include "Mesh.h"
+
+class Model
+{
+public:
+    Model(char* path);
+    void Draw(Shader shader);
+
+private:
+    std::vector<Mesh>   meshes;
+    std::string         directory;
+
+    void loadModel(std::string path);
+    void processNode(aiNode *node, const aiScene *scene);
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
+};
+
+#endif // MODEL_H
