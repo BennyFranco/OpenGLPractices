04c195df83c7b30cfd338682d7067e80ff42402c
Benny Franco Dennis <bennyfranco@icloud.com>
2018-11-01 00:00:08 -0700

    Parallax oclusion mapping

--- 30-ParallaxMapping/shaders/normalMapping.fs
+++ 30-ParallaxMapping/shaders/normalMapping.fs
@@ -39,7 +39,15 @@ vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
         currentLayerDepth += layerDepth;
     }
 
-    return currentTexCoords;
+    // Parallax oclusion mapping extension
+    vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
+    float afterDepth = currentDepthMapValue - currentLayerDepth;
+    float beforeDepth = texture(depthMap, prevTexCoords).r - currentLayerDepth + layerDepth;
+    
+    float weight = afterDepth / (afterDepth - beforeDepth);
+    vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
+
+    return finalTexCoords;  
 } 
 
 void main()
