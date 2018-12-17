3f0fb0d96dd3bad88be3b2307518b6302a214e86
Benny Franco <bennyfranco@icloud.com>
2018-12-17 14:00:58 -0800

    Adding ssao project

---
+++ 34-SSAO/Shader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	GLuint ID;		// the program ID
+
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath, const GLchar* geometryPath = nullptr); // Constructor reads and builds the shader
+	void use(); // use/active the shader
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string &name, int value) const;
+	void setFloat(const std::string &name, float value) const;
+	void setMat4(const std::string &name, glm::mat4 value) const;
+	void setVec3(const std::string &name, glm::vec3 &value) const;
+	void setVec3(const std::string &name, float x, float y, float z) const;
+
+private:
+	void checkCompileErrors(GLuint shader, std::string type, const GLchar* path);
+};
+#endif // SHADER_H
